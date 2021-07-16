ea1b165baf4553e5c63d42dc63d5915725db8c78
Charles <c.friend85@yahoo.com>
2021-07-15 20:41:05 -0600

    fixed Jquery variable

--- Feedster/main.js
+++ Feedster/main.js
@@ -12,16 +12,16 @@ $(document).ready(() => {
     });
 
     $('.postText').on('keyup', event => {
-        let post = $(event.currentTarget).val();
-        let remaining = 140 - post.length;
+        let $post = $(event.currentTarget).val();
+        let $remaining = 140 - $post.length;
 
-        if(remaining <= 0) {
+        if($remaining <= 0) {
             $('.wordcount').addClass('red');
         }
         else {
             $('.wordcount').removeClass('red');
         }
-        $('.characters').html(remaining);
+        $('.characters').html($remaining);
     });
     
     $('.postText').focus();
